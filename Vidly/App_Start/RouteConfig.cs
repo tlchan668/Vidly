b8db203dd092de4204a3fe5b63094a688e218b43
Traci Chan <tlc668@gmail.com>
2022-02-26 15:47:30 -0500

    lesson 14

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -9,12 +9,24 @@ namespace Vidly {
     public class RouteConfig {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           
+            //attribute routing
+            routes.MapMvcAttributeRoutes();
+
+            //custom  Routes
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new {controller = "Movies", action = "ByReleaseDate" },
+            //    //new {year = @"\d{4}", month = @"\d{2}"  //this says to use a year of 4 digits and month with 2
+            //    new {year = @"2015|2016", month = @"\d{2}" //this limits year to 2015 or 2016
+            //    });
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional 
+                });
         }
     }
 }
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,37 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+
+            //passing data by viewData Dictionary !!!Don't Do!!!
+            //ViewData["Movie"] = movie;
+            //ViewBag["Movie"] 
             return View(movie);
+            
+            //return new ViewResult();
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12) }")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit (int id) {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy) {
+            if(!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if(String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortyBy={1}", pageIndex, sortBy));
         }
     }
 }
