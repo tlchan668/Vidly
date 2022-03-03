2b359d9657cfb2dfb1675273a91d435b01dfe1f7
Traci Chan <tlc668@gmail.com>
2022-03-03 16:41:03 -0500

    fixed nav bar to be horizontal and basic movie and customer

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,30 +10,44 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        
+        public ViewResult Index()
         {
+            var movies = GetMovies();
+
+            return View(movies);
+                
+        }
+
+        private IEnumerable<Movie> GetMovies() {
+            return new List<Movie> {
+                new Movie { Id = 1, Name = "Shrek!" },
+                new Movie { Id = 2, Name = "Wall - e"}
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random() {
             var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer> {
-                new Customer { Name = "Customer 1"},
-                new Customer { Name = "Customer 2"}
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
-            var viewModel = new RandomMovieViewModel{
+            var viewModel = new RandomMovieViewModel {
                 Movie = movie,
                 Customers = customers
             };
 
-            //passing data by viewData Dictionary !!!Don't Do!!!
-            //ViewData["Movie"] = movie;
-            //ViewBag["Movie"] 
             return View(viewModel);
-            
-            //return new ViewResult();
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
+
+        //return new ViewResult();
+        //return Content("Hello World!");
+        //return HttpNotFound();
+        //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+    }
 
         //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12) }")]
         //public ActionResult ByReleaseDate(int year, int month)
@@ -55,5 +69,4 @@ namespace Vidly.Controllers
 
         //    return Content(String.Format("pageIndex={0}&sortyBy={1}", pageIndex, sortBy));
         //}
-    }
 }
