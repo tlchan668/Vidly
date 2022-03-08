7f311fa8ff5da0bd46a3b7c0eb9ecb1a377c4ba3
Traci Chan <tlc668@gmail.com>
2022-03-08 10:21:14 -0500

    corrected DBContext

--- Vidly/Models/myData.cs
+++ Vidly/Models/myData.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+//using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Vidly.Models {
     public class MyDbContext : DbContext {
-        public MyDbContext() { }
+        
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }  
     }
 }
