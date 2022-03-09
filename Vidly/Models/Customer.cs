7e251b73cd36c9501e17c7c2b9fbd4bfc0c30e44
Traci Chan <tlc668@gmail.com>
2022-03-09 11:14:07 -0500

    27 seeding MembershipTypes into DB

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -7,5 +7,9 @@ namespace Vidly.Models {
     public class Customer {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType membershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+
     }
 }   
